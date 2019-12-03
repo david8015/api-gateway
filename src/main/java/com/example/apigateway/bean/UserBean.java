f6fd0c28600cb99a0ca8b58b24c8efe761f202e5
David Hernandez <david.hernandez8015@gmail.com>
2019-12-02 21:26:35 -0500

    initial commit

---
+++ src/main/java/com/example/apigateway/bean/UserBean.java
@@ -0,0 +1,36 @@
+package com.example.apigateway.bean;
+
+public class UserBean {
+
+    private Long id;
+
+    private String email;
+
+    private String username;
+
+    private String password;
+
+    public UserBean(Long id, String email, String username, String password) {
+        this.id = id;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+}
---
+++ src/main/java/com/example/apigateway/service/CustomUserService.java
@@ -0,0 +1,43 @@
+package com.example.apigateway.service;
+
+import com.example.apigateway.bean.UserBean;
+import com.example.apigateway.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CustomUserService implements UserDetailsService {
+
+    @Autowired
+    @Qualifier("encoder")
+    PasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        System.out.println("loadUser");
+        UserBean user = userRepository.getUserByUsername(username);
+        System.out.println(user.getUsername());
+        if(user==null)
+            throw new UsernameNotFoundException("User null");
+
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), bCryptPasswordEncoder.encode(user.getPassword()),
+                true, true, true, true, new ArrayList<>());
+    }
+
+    private List<GrantedAuthority> getGrantedAuthorities(UserBean user){
+        List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
+        return authorities;
+    }
+}
